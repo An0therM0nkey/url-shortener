dcdea86b669be897e878d10d2b7931f6c863367e
An0therM0nkey <paul.vo1t3nko@gmail.com>
2020-10-28 20:38:17 +0200

    Reduce short_url length

--- src/models.py
+++ src/models.py
@@ -7,10 +7,14 @@ from sqlalchemy_serializer import SerializerMixin
 from .db import db
 
 
+CHARACTERS = string.ascii_letters + string.digits  # 26 + 26 + 10 = 62
+AMOUNT = 5  # enough for (62! / (5! * (62 - 5)!)) = 6471002 possible combinations
+
+
 class LinksPair(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(1024))
-    short_url = db.Column(db.String(64))
+    short_url = db.Column(db.String(AMOUNT))
     date_created = db.Column(db.DateTime, default=datetime.now())
     days_limit = db.Column(db.Integer, default=90)
 
@@ -23,8 +27,7 @@ class LinksPair(db.Model, SerializerMixin):
         self.short_url = self.generate_short_url()
 
     def generate_short_url(self):
-        characters = string.ascii_letters + string.digits
         while True:
-            short_url = ''.join(random.choice(characters) for i in range(8))
+            short_url = ''.join(random.choice(CHARACTERS) for i in range(AMOUNT))
             if not self.query.filter_by(short_url=short_url).first():
                 return short_url
