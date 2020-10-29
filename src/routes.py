79bb71559f586c8b5c120be511e27e6090c89a5f
An0therM0nkey <paul.vo1t3nko@gmail.com>
2020-10-29 09:19:38 +0200

    Delete expired links pair on redirect attempt.

--- src/routes.py
+++ src/routes.py
@@ -2,7 +2,8 @@ import datetime
 
 from flask import Blueprint, render_template, redirect
 
-from .services import get_all_links_pairs, get_links_pair_by_short_url
+from .services import (get_all_links_pairs, get_links_pair_by_short_url,
+                       delete_links_pair)
 from .serializers import links_pair_frontend_serializer
 
 home = Blueprint('home', __name__)
@@ -22,6 +23,7 @@ def redirect_to_original_url(short_url):
     serialized_pair = links_pair_frontend_serializer(pair)
 
     if serialized_pair['expires_on'] < datetime.datetime.now():
+        delete_links_pair(pair.id)
         return render_template('link_expired.html', links_pair=serialized_pair)
 
     return redirect(serialized_pair['redirect_url'])
